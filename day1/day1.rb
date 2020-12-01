67f161669d750f01002f9a5b244a518425db7107
Keith Hunter <keith.hunter@outlook.com>
2020-12-01 18:32:15 -0500

    day 1 part 2

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,8 +1,16 @@
-def findSum2020AndMultiply(array)
+def findTwoSum2020AndMultiply(array)
   array.each_with_index do |a, index|
-    array.drop(index).each do |b|
-      if a + b == 2020
-        return a * b
+    array.drop(index + 1).each do |b|
+      return a * b if a + b == 2020
+    end
+  end
+end
+
+def findThreeSum2020AndMultiply(array)  
+  array.each_with_index do |a, a_index|                     
+    array.drop(a_index + 1).each_with_index do |b, b_index| 
+      array.drop(a_index + 2 + b_index).each do |c|         
+        return a * b * c if a + b + c == 2020
       end
     end
   end
--- day1/tc_day1.rb
+++ day1/tc_day1.rb
@@ -2,13 +2,23 @@ require_relative "day1"
 require "test/unit"
 
 class TestDay1 < Test::Unit::TestCase
-  def test_example
+  def test_example_part_1
     array = [1721, 979, 366, 299, 675, 1456]
-    assert_equal(514579, findSum2020AndMultiply(array))
+    assert_equal(514579, findTwoSum2020AndMultiply(array))
   end
 
-  def test_input
+  def test_input_part_1
     array = File.read("day1/input.txt").split.map(&:to_i)
-    assert_equal(712075, findSum2020AndMultiply(array))
+    assert_equal(712075, findTwoSum2020AndMultiply(array))
+  end
+
+  def test_example_part_2
+    array = [1721, 979, 366, 299, 675, 1456]
+    assert_equal(241861950, findThreeSum2020AndMultiply(array))
+  end
+
+  def test_input_part_2
+    array = File.read("day1/input.txt").split.map(&:to_i)
+    assert_equal(145245270, findThreeSum2020AndMultiply(array))
   end
 end
