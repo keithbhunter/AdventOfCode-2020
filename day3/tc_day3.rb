d59cced2b568d0e5ad4dee13cce87c137520f2db
Keith Hunter <keith.hunter@outlook.com>
2020-12-03 13:27:45 -0500

    day 3 part 2

--- day3/tc_day3.rb
+++ day3/tc_day3.rb
@@ -23,13 +23,37 @@ class TestDay23 < Test::Unit::TestCase
     graph = Graph.new("day3/example.txt")
     values = graph.values_along_slope(0, 0, 3, 1)
     trees = values.select { |char| char == "#" }
-    assert_equal(7, trees.length)
+    assert_equal(7, number_of_trees(graph, 3, 1))
   end
 
   def test_input_1
     graph = Graph.new("day3/input.txt")
     values = graph.values_along_slope(0, 0, 3, 1)
     trees = values.select { |char| char == "#" }
-    assert_equal(184, trees.length)
+    assert_equal(184, number_of_trees(graph, 3, 1))
+  end
+
+  def test_example_2
+    graph = Graph.new("day3/example.txt")
+    slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+    result = slopes.reduce(1) { |memo, slope|
+      memo * number_of_trees(graph, slope[0], slope[1])
+    }
+    assert_equal(336, result)
+  end
+
+  def test_input_2
+    graph = Graph.new("day3/input.txt")
+    slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+    result = slopes.reduce(1) { |memo, slope|
+      memo * number_of_trees(graph, slope[0], slope[1]) 
+    }
+    assert_equal(2431272960, result)
+  end
+
+  def number_of_trees(graph, slope_x, slope_y)
+    values = graph.values_along_slope(0, 0, slope_x, slope_y)
+    trees = values.select { |char| char == "#" }
+    trees.length
   end
 end
