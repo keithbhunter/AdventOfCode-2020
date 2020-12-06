5f8c509f8475a85a882c566a34823aee82a28081
Keith Hunter <keith.hunter@outlook.com>
2020-12-06 12:00:00 -0500

    day 6 part 2

--- day6/day6.rb
+++ day6/day6.rb
@@ -1,18 +1,31 @@
 class CustomsForms
-  def self.num_of_yes(groups)
-    groups.reduce(0) do |num, form|
-      num + num_of_yes_in_form(form)
+  def self.num_of_yes_from_everyone(groups)
+    yeses = groups.reduce(0) do |sum, form|
+      counts = yes_in_form(form)
+      num_of_groups = form.split("\n").length
+
+      counts = counts.filter do |answer, count|
+        count == num_of_groups
+      end
+
+      sum + counts.length
     end
   end
 
-  def self.num_of_yes_in_form(form)
+  def self.num_of_yes(groups)
+    groups.reduce(0) do |num, form|
+      num + yes_in_form(form).length
+    end
+  end
+
+  def self.yes_in_form(form)
     yes_answers = form
       .delete("\n")
       .split("")
       .reduce({}) do |answers, char|
-        answers[char] = true
+        answers[char] = answers[char] ? answers[char] + 1 : 1
         answers
       end
-    yes_answers.length
+    yes_answers
   end
 end
--- day6/tc_day6.rb
+++ day6/tc_day6.rb
@@ -4,19 +4,31 @@ require "test/unit"
 class TestDay6 < Test::Unit::TestCase
   def test_num_of_yes_in_form
     groups = File.read("day6/example.txt").split("\n\n")
-    num = CustomsForms.num_of_yes_in_form(groups.first)
+    num = CustomsForms.yes_in_form(groups.first).length
     assert_equal(3, num)
   end
 
-  def test_num_of_yes
+  def test_num_of_yes_example
     groups = File.read("day6/example.txt").split("\n\n")
     num = CustomsForms.num_of_yes(groups)
     assert_equal(11, num)
   end
 
-  def test_num_of_yes
+  def test_num_of_yes_input
     groups = File.read("day6/input.txt").split("\n\n")
     num = CustomsForms.num_of_yes(groups)
     assert_equal(6382, num)
+  end
+
+  def test_num_of_yes_from_everyone_example
+    groups = File.read("day6/example.txt").split("\n\n")
+    num = CustomsForms.num_of_yes_from_everyone(groups)
+    assert_equal(6, num)
+  end
+
+  def test_num_of_yes_from_everyone_input
+    groups = File.read("day6/input.txt").split("\n\n")
+    num = CustomsForms.num_of_yes_from_everyone(groups)
+    assert_equal(3197, num)
   end
 end
